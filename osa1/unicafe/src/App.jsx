1e57360680acecbddefedde2bab7964608336f8a
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2024-02-27 01:46:26 +0200

    unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,21 +1,42 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const handleGood = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBad = () => {
+        setBad(bad + 1)
+    }
+
     return (
         <div>
             <h1>give feedback</h1>
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <Button handleClick={handleGood} text='good' />
+            <Button handleClick={handleNeutral} text='neutral' />
+            <Button handleClick={handleBad} text='bad' />
 
             <h1>statistics</h1>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {bad + neutral + good}</p>
+            <p>average {(-bad + good) / (bad + neutral + good)}</p>
+            <p>positive {good / (bad + neutral + good) * 100} %</p>
         </div>
     )
 }
