bc49effe78a8700fa3c2014c146e492e2f3bb017
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2024-03-04 01:19:34 +0200

    erillinen moduuli

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <strong>total of {sum} exercises</strong>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-      <Total sum={total} />
-    </>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content key={course.id} parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
