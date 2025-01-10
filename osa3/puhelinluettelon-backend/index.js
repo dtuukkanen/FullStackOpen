73f59f8fc1d3cc6070237021978adaff95aa19ef
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-01-10 18:02:33 +0200

    3.2 puhelinluettelon backend step2

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -29,6 +29,14 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
