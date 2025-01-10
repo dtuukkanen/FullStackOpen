605f1f8e3e72e305c94999b9970ddd033c03eba5
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-01-10 20:11:59 +0200

    3.4: puhelinluettelon backend step4

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -45,6 +45,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
 });
 
 app.listen(PORT, () => {
